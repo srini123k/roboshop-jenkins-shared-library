f26bb8cfd23e3a296a1cfb52d14c1650317cfc8a
Srini K <srinikdevops0131@gmail.com>
2023-04-03 15:36:02 -0700

    updating ./roboshop-jenkins-shared-library

---
+++ vars/terraform.groovy
@@ -0,0 +1,33 @@
+def call() {
+    pipeline {
+        agent any
+
+//        parameters {
+//            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+//        }
+//
+//        options {
+//            ansiColor('xterm')
+//        }
+
+
+        stages {
+
+            stage('Init') {
+                steps {
+                    sh 'terraform init -backend-config=env-dev/main.tfvars'
+                    // '${ENV}/state.tfvars'
+                }
+            }
+
+            stage('Apply') {
+                steps {
+                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                    // sh 'echo'
+                }
+            }
+
+        }
+
+    }
+}
