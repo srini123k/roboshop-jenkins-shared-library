b334fc8c84b54129e5eae4d3511246f3dc0bccba
Srini K <srinikdevops0131@gmail.com>
2023-04-04 12:28:17 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -4,6 +4,7 @@ def call() {
 
          parameters {
             string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+             string(name: 'ACTION', defaultValue: '', description: 'Which Action?')
         }
 
         options {
@@ -23,8 +24,8 @@ def call() {
 
             stage('Apply') {
                 steps {
-                    sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
-                    // sh 'echo'
+                    // sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
+                     sh 'echo'
                 }
             }
 
