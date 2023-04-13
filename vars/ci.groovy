55edc54930e1439cfcdb153ec307bce75ca46de4
Srini K <srinikdevops0131@gmail.com>
2023-04-12 22:01:55 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -14,7 +14,7 @@ def call() {
 
             stage('Check Out Code') {
                 cleanWs()
-                git branch: 'main', url: 'https://github.com/srini123k/cart'
+                git branch: 'main', url: "https://github.com/srini123k/${component}"
             }
 
             sh 'env'
