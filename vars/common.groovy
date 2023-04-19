641a3a66b8b0aca034b0dca4adeef3896fd4741a
Srini K <srinikdevops0131@gmail.com>
2023-04-19 06:42:47 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -35,9 +35,12 @@ def prepareArtifacts() {
 
 
 def artifactUpload() {
-    sh 'echo ${TAG_NAME} >VERSION'
-    if (app_lang == "nodejs" || app_lang == "angular") {
-        sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.8.203:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+    env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    wrap([$class: 'MaskPasswordsBuildWrapper',
+          varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
+        sh 'echo ${TAG_NAME} >VERSION'
+        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.8.203:8081/repository/${component}/${component}-${TAG_NAME}.zip'
 
     }
 }
