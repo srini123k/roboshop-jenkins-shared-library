043a83b7214d1510d1e3638d8ce81436cd92c2c1
Srini K <srinikdevops0131@gmail.com>
2023-04-28 11:40:14 -0700

    updating ./roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -31,19 +31,19 @@ def prepareArtifacts() {
 //        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
 //    }
     sh 'docker build -t 922057391995.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
-
+ //   922057391995.dkr.ecr.us-east-1.amazonaws.com/frontend
 }
 
 
 def artifactUpload() {
-//    env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
-//    env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
-//    wrap([$class: 'MaskPasswordsBuildWrapper',
-//          varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
-//        sh 'echo ${TAG_NAME} >VERSION'
-//        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.8.203:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-//
-//    }
+    env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    wrap([$class: 'MaskPasswordsBuildWrapper',
+          varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
+        sh 'echo ${TAG_NAME} >VERSION'
+        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.8.203:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+
+    }
 //    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 922057391995.dkr.ecr.us-east-1.amazonaws.com'
 //    sh 'docker push 922057391995.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
 
